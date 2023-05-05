a89d9791a4180574d8dec819a807aa3e7c317716
kuanchiliao1 <kuanchiliao@gmail.com>
2023-05-05 16:22:09 -0400

    Implement renderProjects()

--- src/ScreenController.js
+++ src/ScreenController.js
@@ -6,33 +6,54 @@ const ScreenController = () => {
   const projectlist = ProjectListFactory();
   projectlist.addProject();
 
+  const currentActiveProject = () =>
+    projectlist.find((project) => project.active);
+
   const updateScreen = () => {
-    const contentDiv = document.getElementById("content");
+    const contentDiv = document.getElementById('content');
+    renderProjects();
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    // // Add the image to our existing div.
+    // const myIcon = new Image();
+    // myIcon.src = Icon;
 
-    contentDiv.appendChild(myIcon);
+    // contentDiv.appendChild(myIcon);
   };
 
   const renderProjects = () => {
+    const projects = projectlist.getProjects();
+    projects.forEach((project) => {
+      const projectDiv = document.createElement('div');
+      const projectContainerDiv = document.querySelector('.projects-container');
+
+      projectDiv.classList.add('project');
+      projectDiv.textContent = project.name;
+      projectContainerDiv.append(projectDiv);
+    });
+    console.log(projectlist.getProjects());
+  };
+
+  const renderTodos = () => {
     projectlist.getProjects();
   };
 
   const initialRender = () => {
     const contentDiv = document.createElement('div');
-    contentDiv.id = "content";
+    contentDiv.id = 'content';
     contentDiv.innerHTML = `
-      <div class="project-container"></div>
-      <div class="todo-container hello"></div>
+      <div class="projects-container">
+      </div>
+      </div>
+      <div class="todos-container">
+        <h1>Todo</h1>
+      </div>
     `;
-    document.body.append(contentDiv)
-  }
+    document.body.append(contentDiv);
+  };
 
   const bindEventListeners = () => {};
 
-  initialRender()
+  initialRender();
   updateScreen();
 };
 
