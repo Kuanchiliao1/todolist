e29cbf2bc7c35daca46df2257060d4b21d72bf58
kuanchiliao1 <kuanchiliao@gmail.com>
2023-05-14 23:38:02 -0400

    Add font awesome icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './style.css';
 import _ from 'lodash';
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faX } from '@fortawesome/free-solid-svg-icons/faX';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
 import TodoFactory from './TodoFactory';
 import ProjectFactory from './ProjectFactory';
 import ProjectListFactory from './ProjectListFactory';
 import ScreenController from './ScreenController';
 
-library.add(faCheck);
+library.add(faCheck, faPlus, faX, faEdit);
 dom.watch();
 
 ScreenController();
